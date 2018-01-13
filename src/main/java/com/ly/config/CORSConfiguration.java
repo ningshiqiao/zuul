e810a2013a7feb392a27f194f9207099bba32794
钟伟 <dongfang.x@qq.com>
2018-01-13 18:12:19 +0800

    zuul 接口配置

--- src/main/java/com/ly/config/CORSConfiguration.java
+++ src/main/java/com/ly/config/CORSConfiguration.java
@@ -11,18 +11,15 @@ import org.springframework.web.filter.CorsFilter;
  */
 @Configuration
 public class CORSConfiguration {
-    private CorsConfiguration buildConfig() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*"); // 1
-        corsConfiguration.addAllowedHeader("*"); // 2
-        corsConfiguration.addAllowedMethod("*"); // 3
-        return corsConfiguration;
-    }
-
     @Bean
     public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", buildConfig()); // 4
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
 }
