abcb7881b2860a8f4cc510834e177bf42a147700
钟伟 <dongfang.x@qq.com>
2018-01-14 19:24:46 +0800

    del cros

--- src/main/java/com/ly/config/CORSConfiguration.java
+++ src/main/java/com/ly/config/CORSConfiguration.java
@@ -9,13 +9,13 @@ import org.springframework.web.filter.CorsFilter;
 /**
  * Created by zhongwei on 22/03/2017.
  */
-@Configuration
+//@Configuration
 public class CORSConfiguration {
     @Bean
     public CorsFilter corsFilter() {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
+//        config.setAllowCredentials(true);
         config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
