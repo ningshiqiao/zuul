a9040a2da7073356e8b07366f0784e5b927affea
lianzeng <lianzeng@9fbank.cc>
2018-04-04 10:10:46 +0800

    路由获取角色id

--- src/main/java/com/ly/filter/AccessFilter.java
+++ src/main/java/com/ly/filter/AccessFilter.java
@@ -187,7 +187,7 @@ public class AccessFilter extends ZuulFilter{
 
                 ctx.addZuulRequestHeader(Global.USER_ID, claims.get(Global.USER_ID).toString());
                 ctx.addZuulRequestHeader(Global.PHONE, claims.get(Global.PHONE).toString());
-                if(null==claims.get(Global.ROLE_ID)){
+                if(null!=claims.get(Global.ROLE_ID)){
                     ctx.addZuulRequestHeader(Global.ROLE_ID, claims.get(Global.ROLE_ID).toString());
                 }
             }catch (Exception e){
