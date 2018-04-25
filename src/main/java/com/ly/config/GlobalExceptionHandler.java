e0d427c8af5bbf1d2978911050ec99412e44312c
张为 <zhangwei6@9fbank.cc>
2018-04-25 14:25:45 +0800

    全局异常

---
+++ src/main/java/com/ly/config/GlobalExceptionHandler.java
@@ -0,0 +1,41 @@
+package com.ly.config;
+
+import com.ly.helper.AppException;
+import com.ly.helper.Result;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(value = Exception.class)
+    public Result defaultErrorHandler(HttpServletRequest req, Exception exception) throws Exception {
+        logger.info("=========Exception[begin]==========");
+        StringBuilder sb = new StringBuilder(req.getRequestURL().toString());
+        if (exception instanceof AppException) {
+            AppException e = (AppException) exception;
+            sb.append("  CODE:");
+            sb.append(e.getErrorCode().getCode().toString());
+            sb.append("  MESSAGE:");
+            sb.append(e.getErrorCode().getMessage().toString());
+            logger.error(sb.toString());
+            return new Result(e.getErrorCode().getCode(), e.getErrorCode().getMessage());
+        } else {
+            sb.append("  CODE:");
+            sb.append("");
+            sb.append("  MESSAGE:");
+            sb.append(exception.getMessage().toString());
+            logger.error(sb.toString());
+            return new Result(Result.Status.ERROR,"",exception.getMessage().toString());
+        }
+    }
+
+}
+
---
+++ src/main/java/com/ly/helper/AppException.java
@@ -0,0 +1,18 @@
+package com.ly.helper;
+
+public class AppException extends Exception {
+
+    private ErrorCode errorCode;
+
+    public AppException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+}
